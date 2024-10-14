c5072cf174c30fe7ffa29bb66ab1403752ce71a8
mohamedelhewehy <m.osama_el7ewehy@yahoo.com>
2024-10-14 17:37:58 +0300

    structure the app

---
+++ lib/widgets/adaptive_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveLayout extends StatelessWidget {
+  const AdaptiveLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
