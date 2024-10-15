dca12287d839c9ce3deccdbcbe7cac20ced884bf
mohamedelhewehy <m.osama_el7ewehy@yahoo.com>
2024-10-15 20:14:33 +0300

    genereate type adaptor

--- lib/features/presentation/data/model/task_model.dart
+++ lib/features/presentation/data/model/task_model.dart
@@ -1,6 +1,13 @@
-class TaskModel {
+import 'package:hive/hive.dart';
+part 'task_model.g.dart'; //to genereate type adaptor
+
+@HiveType(typeId: 0)
+class TaskModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final DateTime dueDate;
+  @HiveField(2)
   final bool isDone;
 
   TaskModel({required this.title, required this.dueDate, required this.isDone});
